7f92f6692d349818fa8c97585740ac692ea6b928
toru-bayashi <laica12_breese17@hotmail.com>
2015-03-26 23:42:59 +0900

    [memo] 必要な機能、開発フェーズと実装内容を仮定義（未レビュー）

---
+++ _waka_draft/Koemon_mindmap.mm
@@ -0,0 +1,195 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1427256338994" ID="ID_1893706870" MODIFIED="1427257763024" TEXT="&#x30b3;&#x30a8;&#x30e2;&#x30f3;">
+<font NAME="Meiryo UI" SIZE="20"/>
+<icon BUILTIN="gohome"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1427257348741" ID="ID_1141961219" MODIFIED="1427360654027" POSITION="right" TEXT="Ver0.1">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Meiryo UI" SIZE="18"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#00b439" CREATED="1427257348741" ID="ID_1390498242" MODIFIED="1427360659065" TEXT="Ver0.1.0">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="16"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#990000" CREATED="1427256351686" ID="ID_755109926" MODIFIED="1427359760778" TEXT="&#x97f3;&#x30c7;&#x30fc;&#x30bf;&#x8aad;&#x307f;&#x8fbc;&#x307f;&#x2192;&#x753b;&#x50cf;&#x30c7;&#x30fc;&#x30bf;&#x66f8;&#x304d;&#x51fa;&#x3057;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="14"/>
+<icon BUILTIN="edit"/>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_1633802243" MODIFIED="1427359760778" TEXT="WAV&#x30d5;&#x30a1;&#x30a4;&#x30eb;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427257511451" ID="ID_501596974" MODIFIED="1427360514424" TEXT="&#x8aad;&#x307f;&#x8fbc;&#x307f;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node COLOR="#111111" CREATED="1427360408468" ID="ID_1758675114" MODIFIED="1427360422917" TEXT="WAV&#x30d5;&#x30a1;&#x30a4;&#x30eb;&#x30c7;&#x30fc;&#x30bf;&#x306e;&#x8aad;&#x307f;&#x8fbc;&#x307f;"/>
+</node>
+<node COLOR="#111111" CREATED="1427257511451" ID="ID_839489176" MODIFIED="1427360467350" TEXT="&#x53d6;&#x5f97;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node COLOR="#111111" CREATED="1427258512321" ID="ID_841176766" MODIFIED="1427360450653" TEXT="&#x30c7;&#x30fc;&#x30bf;&#x30b5;&#x30a4;&#x30ba;"/>
+<node COLOR="#111111" CREATED="1427258526137" ID="ID_970781243" MODIFIED="1427360448109" TEXT="&#x30d3;&#x30c3;&#x30c8;&#x30ec;&#x30fc;&#x30c8;"/>
+<node COLOR="#111111" CREATED="1427258530786" ID="ID_683374035" MODIFIED="1427360446054" TEXT="&#x30b5;&#x30f3;&#x30d7;&#x30ea;&#x30f3;&#x30b0;&#x5468;&#x6ce2;&#x6570;"/>
+<node COLOR="#111111" CREATED="1427258536778" ID="ID_1192911861" MODIFIED="1427258551498" TEXT="&#x30e2;&#x30ce;&#x30e9;&#x30eb;&#xff0f;&#x30b9;&#x30c6;&#x30ec;&#x30aa;"/>
+</node>
+<node COLOR="#111111" CREATED="1427258570276" ID="ID_163525453" MODIFIED="1427360486775" TEXT="&#x62bd;&#x51fa;">
+<icon BUILTIN="button_ok"/>
+<node COLOR="#111111" CREATED="1427360488152" ID="ID_1026119706" MODIFIED="1427360492952" TEXT="PCM&#x30c7;&#x30fc;&#x30bf;"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_1110900478" MODIFIED="1427359760780" TEXT="BMP&#x30d5;&#x30a1;&#x30a4;&#x30eb;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427257511451" ID="ID_205009569" MODIFIED="1427258048067" TEXT="&#x66f8;&#x304d;&#x51fa;&#x3057;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node COLOR="#111111" CREATED="1427266404333" ID="ID_1789733590" MODIFIED="1427266415733" TEXT="256*256"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_203379084" MODIFIED="1427359760780" TEXT="&#x97f3;&#x2192;&#x753b;&#x50cf;&#x5909;&#x63db;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427258642479" ID="ID_1191438641" MODIFIED="1427360535214" TEXT="PCM&#x30c7;&#x30fc;&#x30bf;&#x2192;RGB&#x30c7;&#x30fc;&#x30bf;">
+<icon BUILTIN="button_ok"/>
+<node COLOR="#111111" CREATED="1427258701386" ID="ID_1745111706" MODIFIED="1427258744617" TEXT="&#xff11;&#x30d0;&#x30a4;&#x30c8;&#x5206;&#x305a;&#x3064;&#x9806;&#x3005;&#x306b;&#x30b3;&#x30d4;&#x30fc;"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1427257348741" ID="ID_404802611" MODIFIED="1427359814526" TEXT="Ver0.1.1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="16"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#990000" CREATED="1427256351686" ID="ID_1266675963" MODIFIED="1427359927976" TEXT="&#x753b;&#x50cf;&#x51fa;&#x529b;&#x30ed;&#x30b8;&#x30c3;&#x30af;&#x6539;&#x826f;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="14"/>
+<icon BUILTIN="edit"/>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_162099261" MODIFIED="1427360238779" TEXT="WAV&#x30d5;&#x30a1;&#x30a4;&#x30eb;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+</node>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_451050432" MODIFIED="1427359760780" TEXT="BMP&#x30d5;&#x30a1;&#x30a4;&#x30eb;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+</node>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_352323935" MODIFIED="1427359760780" TEXT="&#x97f3;&#x2192;&#x753b;&#x50cf;&#x5909;&#x63db;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1427257348741" ID="ID_8827179" MODIFIED="1427363324213" TEXT="Ver0.1.2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="16"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#990000" CREATED="1427256351686" ID="ID_1352198610" MODIFIED="1427363314287" TEXT="&#x753b;&#x50cf;&#x51fa;&#x529b;&#x30ed;&#x30b8;&#x30c3;&#x30af;&#x6539;&#x826f;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="14"/>
+<icon BUILTIN="edit"/>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_88326635" MODIFIED="1427360238779" TEXT="WAV&#x30d5;&#x30a1;&#x30a4;&#x30eb;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427258570276" ID="ID_1483834891" MODIFIED="1427360236829" TEXT="&#x30b9;&#x30c6;&#x30ec;&#x30aa;&#x306e;&#x6ce2;&#x5f62;&#x30c7;&#x30fc;&#x30bf;&#x3092;L/R&#x5225;&#x306b;&#x62bd;&#x51fa;">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#12514;&#12494;&#12521;&#12523;&#12399;0.1.0&#12398;&#27874;&#24418;&#12487;&#12540;&#12479;&#25277;&#20986;&#12398;&#27231;&#33021;&#12381;&#12398;&#12414;&#12414;
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_560446292" MODIFIED="1427359760780" TEXT="BMP&#x30d5;&#x30a1;&#x30a4;&#x30eb;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427360864568" ID="ID_750893372" MODIFIED="1427360924424" TEXT="LR&#x5225;&#x306b;&#x30d4;&#x30af;&#x30bb;&#x30eb;&#x914d;&#x7f6e;&#x3092;&#x533a;&#x5225;">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_38446226" MODIFIED="1427359760780" TEXT="&#x97f3;&#x2192;&#x753b;&#x50cf;&#x5909;&#x63db;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427360864568" ID="ID_672382658" MODIFIED="1427363170806" TEXT="&#x5468;&#x6ce2;&#x6570;&#xff0f;&#x6210;&#x5206;&#x91cf;&#x2192;&#x8272;&#x5ea6;&#x5909;&#x63db;">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_518988412" MODIFIED="1427360752642" TEXT="&#x30b9;&#x30da;&#x30af;&#x30c8;&#x30eb;&#x89e3;&#x6790;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427360757595" ID="ID_1180580854" MODIFIED="1427360787978" TEXT="&#x5468;&#x6ce2;&#x6570;&#x6210;&#x5206;&#x306e;&#x53d6;&#x5f97;">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1427257372886" FOLDED="true" ID="ID_1097712383" MODIFIED="1427360736545" POSITION="right" TEXT="Ver0.2">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Meiryo UI" SIZE="18"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#00b439" CREATED="1427257372886" ID="ID_1865407220" MODIFIED="1427359858436" TEXT="Ver0.2.0">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="16"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#990000" CREATED="1427256410893" ID="ID_27671722" MODIFIED="1427359844493" TEXT="&#x753b;&#x50cf;&#x8aad;&#x307f;&#x8fbc;&#x307f;&#x30c7;&#x30fc;&#x30bf;&#x2192;&#x97f3;&#x30c7;&#x30fc;&#x30bf;&#x66f8;&#x304d;&#x51fa;&#x3057;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="14"/>
+<icon BUILTIN="edit"/>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_903628842" MODIFIED="1427359844494" TEXT="BMP&#x30d5;&#x30a1;&#x30a4;&#x30eb;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427257511451" ID="ID_752666751" MODIFIED="1427353033399" TEXT="&#x753b;&#x50cf;&#x30c7;&#x30fc;&#x30bf;&#x62bd;&#x51fa;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<node COLOR="#111111" CREATED="1427266404333" ID="ID_847508069" MODIFIED="1427266415733" TEXT="256*256"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1427257548565" ID="ID_945840677" MODIFIED="1427359844494" TEXT="WAV&#x30d5;&#x30a1;&#x30a4;&#x30eb;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node COLOR="#111111" CREATED="1427257511451" ID="ID_1469961107" MODIFIED="1427353089323" TEXT="&#x66f8;&#x304d;&#x51fa;&#x3057;">
+<font NAME="Meiryo UI" SIZE="12"/>
+<icon BUILTIN="idea"/>
+<node COLOR="#111111" CREATED="1427353096005" ID="ID_574786281" MODIFIED="1427353136774" TEXT="44.1kHz/16bit/stereo"/>
+<node COLOR="#111111" CREATED="1427353172992" ID="ID_1593250505" MODIFIED="1427353298692" TEXT="&#x6ce2;&#x5f62;&#x306e;&#x9577;&#x3055;&#x306f;&#x56fa;&#x5b9a;(256*256&#x30b5;&#x30a4;&#x30ba;&#x753b;&#x50cf;&#x304b;&#x3089;&#x306e;&#x751f;&#x6210;&#x306e;&#x305f;&#x3081;)"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1427257452979" FOLDED="true" ID="ID_1266978592" MODIFIED="1427360650492" POSITION="right" TEXT="Ver0.3">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Meiryo UI" SIZE="18"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#00b439" CREATED="1427257452979" ID="ID_1364236894" MODIFIED="1427360614805" TEXT="Ver0.3.0">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="16"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#990000" CREATED="1427256436406" ID="ID_1984622334" MODIFIED="1427360614806" TEXT="&#x97f3;&#x58f0;&#x5165;&#x529b;&#x2192;&#x30ea;&#x30a2;&#x30eb;&#x30bf;&#x30a4;&#x30e0;&#x753b;&#x50cf;&#x51fa;&#x529b;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="14"/>
+<icon BUILTIN="edit"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1427257469210" FOLDED="true" ID="ID_1136090110" MODIFIED="1427360651068" POSITION="right" TEXT="Ver0.4">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Meiryo UI" SIZE="18"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#00b439" CREATED="1427257469210" ID="ID_931115471" MODIFIED="1427360627966" TEXT="Ver0.4.0">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="16"/>
+<icon BUILTIN="pencil"/>
+<node COLOR="#990000" CREATED="1427257094275" ID="ID_1119098032" MODIFIED="1427360627966" TEXT="&#x8272;&#x60c5;&#x5831;&#x5165;&#x529b;&#x2192;&#x97f3;&#x58f0;&#x51fa;&#x529b;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="Meiryo UI" SIZE="14"/>
+<icon BUILTIN="edit"/>
+</node>
+</node>
+</node>
+</node>
+</map>
