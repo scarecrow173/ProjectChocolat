b2c2b0d4e127a78e7e91b4e145d8cf6e538ebfbb
AkiyoshiKoyama <akisyohi.koyama@gmail.com>
2015-03-29 00:48:02 +0900

    [Environment] Add SublimeProject for Premake [liblary]Add Audio Base Code

---
+++ Tools/premake/Solutions/ProjectChocolat/Audio.lua
@@ -0,0 +1,22 @@
+-- Audio  --
+module("Audio", package.seeall)
+local ProjectName = "Audio" 
+local DebugDefines =
+{
+	"DEBUG"
+}
+local ReleaseDefines =
+{
+	"NDEBUG"
+}
+local  LinkProjects =
+{
+	"Core"
+}
+make_API.SetupProject (ProjectName)
+make_API.SettingCppStaticLibProject()
+make_API.SettingCppFiles(ProjectName)
+--Debug Setting--
+make_API.SetupConfiguration("Debug", DebugDefines, "lib", "",  "", LinkProjects, "", "Symbols")
+--Release Setting--
+make_API.SetupConfiguration("Release", ReleaseDefines, "lib", "",  "", LinkProjects, "", "Optimize")
